dc290a7e1fc9aa6855af9d668a67e382919cb7cb
Alex Saye <alexandersaye@live.co.uk>
2023-07-23 10:27:48 +0100

    Add PID Corrector

---
+++ Assets/Scripts/Utils/Corrector.cs
@@ -0,0 +1,51 @@
+using System;
+
+/// <summary>
+/// Provides continuous error correction through a PID controller.
+/// </summary>
+[Serializable]
+public class Corrector
+{
+  private float accumulatedError = 0f;
+  private float previousError = 0f;
+
+  /// <summary>
+  /// Calculate the next error correction.
+  /// </summary>
+  public float Next(float error, CorrectorTuning tuning)
+  {
+    // Correct for the present error.
+    var proportional = error * tuning.Proportional;
+
+    // Correct for the accumulated past errors.
+    this.accumulatedError += error;
+    var integral = this.accumulatedError * tuning.Integral;
+
+    // Correct for the predicted future error.
+    var derivative = (error - this.previousError) * tuning.Derivative;
+    this.previousError = error;
+
+    return proportional + integral + derivative;
+  }
+}
+
+/// <summary>
+/// PID tuning for a <see cref="Corrector"/>.
+/// </summary>
+[Serializable]
+public struct CorrectorTuning
+{  /// <summary>
+   /// The influence that the present error has on feedback.
+   /// </summary>
+  public float Proportional;
+
+  /// <summary>
+  /// The influence that the accumulated past errors have on feedback.
+  /// </summary>
+  public float Integral;
+
+  /// <summary>
+  /// The influence that the predicted future error has on feedback.
+  /// </summary>
+  public float Derivative;
+}
